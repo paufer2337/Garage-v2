eeafd8ad8eb697c84b3cf4cb124888b1182333be
PAO <paufer2337@gmail.com>
2026-05-27 21:12:30 +0200

    tests/ added 2 more AddVehicle tests; IncreaseCount and full capacity test

--- tests/App.Tests/GarageTests.cs
+++ tests/App.Tests/GarageTests.cs
@@ -32,6 +32,7 @@ public class GarageTests
     {
         //arrange
         Garage<Vehicle> garage = new(3);
+        Car car = new("ABC123", "Red", 4, "Gasoline");
 
         // act 
         bool result = garage.AddVehicle(car);
@@ -39,4 +40,39 @@ public class GarageTests
         // assert
         Assert.True(result);
     }
+
+    [Fact]
+    public void AddVehicle_WhenAdded_IncreaseCount()
+    {
+        //arrange
+        Garage<Vehicle> garage = new(3);
+        Car car = new("ABC123", "Red", 4, "Gasoline");
+
+        //act
+        garage.AddVehicle(car);
+
+        // assert
+        Assert.Equal(1, garage.Count());
+
+    }
+
+    [Fact]
+    public void AddVehicle_IfGarageFull_ReturnFalse()
+    {
+        // arrange
+        Garage<Vehicle> garage = new(1);
+
+        Car firstCar = new("ABC123", "Red", 4, "Gasoline");
+        Car secondCar = new("ABC456", "Black", 4, "Diesel");
+
+        garage.AddVehicle(firstCar);
+
+        // act
+        bool result = garage.AddVehicle(secondCar);
+        
+        // assert
+        Assert.False(result);
+        Assert.Equal(1, garage.Count());
+    }
+    
 }
